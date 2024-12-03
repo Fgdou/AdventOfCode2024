eb1ae067875a1429c78c98b40679792cdf4853f3
Fgdou <fabigoardou@gmail.com>
2024-12-03 19:22:07 +0000

    D01 P2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,13 +1,11 @@
 advent_of_code::solution!(1);
 
-pub fn part_one(input: &str) -> Option<i32> {
-    // transformer en 2 listes
-
+pub fn parse_input(input: &str) -> (Vec<i32>, Vec<i32>) {
     let numbers: Vec<Vec<i32>> = input.split("\n")
-        .filter(|line| !line.is_empty())
-        .map(|line| line.split_whitespace()
-            .map(|n| n.parse().unwrap()).collect()
-        ).collect();
+    .filter(|line| !line.is_empty())
+    .map(|line| line.split_whitespace()
+        .map(|n| n.parse().unwrap()).collect()
+    ).collect();
 
     let mut list1 = Vec::new();
     let mut list2 = Vec::new();
@@ -15,6 +13,14 @@ pub fn part_one(input: &str) -> Option<i32> {
         list1.push(list[0]);
         list2.push(list[1]);
     }
+
+    (list1, list2)
+}
+
+pub fn part_one(input: &str) -> Option<i32> {
+    // transformer en 2 listes
+    let (mut list1, mut list2) = parse_input(input);
+    
 
     // trie les listes
     list1.sort();
@@ -29,8 +35,19 @@ pub fn part_one(input: &str) -> Option<i32> {
     Some(list_result.sum())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn count_iteration(list: &Vec<i32>, number: i32) -> i32 {
+    list.iter().filter(|other_number| number == **other_number).count() as i32
+}
+
+pub fn part_two(input: &str) -> Option<i32> {
+    // on veut nos 2 listes
+    let (list1, list2) = parse_input(input);
+    // pour chaque nombre de gauche, compter à droite et multiplier
+    let result_list = list1.iter().map(|number| 
+        number * count_iteration(&list2, *number)
+    );
+
+    Some(result_list.sum())
 }
 
 #[cfg(test)]
@@ -46,6 +63,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
