79d3b4563e2880af90e9b5ea713e1aa3de0536c7
Fgdou <fabigoardou@gmail.com>
2024-12-06 18:59:23 +0000

    D06P2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -4,7 +4,7 @@ use vectorlib::math::vector2d_module::Vector2d;
 
 advent_of_code::solution!(6);
 
-#[derive(Debug)]
+#[derive(Debug, Eq, Hash, PartialEq, Clone)]
 enum Direction {
     UP,
     LEFT,
@@ -63,7 +63,7 @@ impl Guard {
     }
 }
 
-fn is_inside(map_size: Vector2d<i32>, pos: Vector2d<i32>) -> bool {
+fn is_inside(map_size: &Vector2d<i32>, pos: Vector2d<i32>) -> bool {
     pos.x >= 0 && pos.y >= 0 && pos.x < map_size.x && pos.y < map_size.y
 }
 
@@ -107,7 +107,7 @@ pub fn part_one(input: &str) -> Option<usize> {
 
     let mut history = HashSet::new();
 
-    while is_inside(input.map_size, guard.position) {
+    while is_inside(&input.map_size, guard.position) {
         history.insert((guard.position.x, guard.position.y));
         guard.walk(&input.obstacles);
     }
@@ -115,8 +115,52 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(history.len())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+fn is_infinite_loop(map_size: &Vector2d<i32>, start_pos: &Vector2d<i32>, obstacles: &Obstacles) -> bool {
+    let mut guard = Guard {
+        position: start_pos.clone(),
+        direction: Direction::UP,
+    };
+
+    let mut history = HashSet::new();
+
+    while is_inside(map_size, guard.position) {
+        let entry = ((guard.position.x, guard.position.y), guard.direction.clone());
+        if history.contains(&entry) {
+            return true;
+        }
+        history.insert(entry);
+        guard.walk(obstacles);
+    }
+
+    false
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let input = parse(input);
+
+    // get history
+    let mut guard = Guard{
+        position: input.guard_start.clone(),
+        direction: Direction::UP,
+    };
+
+    let mut history = HashSet::new();
+
+    while is_inside(&input.map_size, guard.position) {
+        history.insert((guard.position.x, guard.position.y));
+        guard.walk(&input.obstacles);
+    }
+
+    history.remove(&(input.guard_start.x, input.guard_start.y));
+
+    // run for each position
+    let count = history.iter().filter(|(x, y)| {
+            let mut obstacles = input.obstacles.clone();
+            obstacles.insert((*x, *y));
+            is_infinite_loop(&input.map_size, &input.guard_start, &obstacles)
+        }).count();
+
+    Some(count)
 }
 
 #[cfg(test)]
@@ -132,6 +176,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
