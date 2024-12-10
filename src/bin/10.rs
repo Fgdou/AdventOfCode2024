6ea56d2410e36c63cd173520abe2d1d945370ec1
Fgdou <fabigoardou@gmail.com>
2024-12-10 18:45:06 +0000

    D10P2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -60,6 +60,49 @@ fn find_paths(map: &Map) -> Vec<(Vector2d<i32>, Possibilities)> {
         .collect()
 }
 
+fn next2(requested_number: u32, pos: &Vector2d<i32>, map: &Map, map_size: &Vector2d<i32>) -> Option<usize> {
+    let n = *map.get(pos.y as usize)?.get(pos.x as usize)?;
+    if requested_number != n {
+        return None;
+    }
+    if n == 9 {
+        return Some(1);
+    }
+
+    let dirs = vec!(
+        Vector2d::new(-1, 0),
+        Vector2d::new(1, 0),
+        Vector2d::new(0, -1),
+        Vector2d::new(0, 1),
+    );
+
+    let res: usize = dirs.into_iter().filter_map(|dir| {
+        let nextpos = *pos + dir;
+        next2(requested_number+1, &nextpos, map, map_size)
+    }).sum();
+
+    if res == 0 {
+        None
+    } else {
+        Some(res)
+    }
+}
+
+fn find_paths2(map: &Map) -> usize {
+    let map_size = Vector2d::new(
+        map.get(0).unwrap().len() as i32, 
+        map.len() as i32
+    );
+
+    iproduct!(0..map_size.x, 0..map_size.y)
+        .map(|(x, y)| Vector2d::new(x, y))
+        .filter_map(|pos| {
+            next2(0, &pos, map, &map_size)
+        })
+        .sum()
+}
+
+
 pub fn part_one(input: &str) -> Option<usize> {
     let input = parse(input);
     let paths = find_paths(&input);
@@ -67,8 +110,10 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(count)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let input = parse(input);
+    let count = find_paths2(&input);
+    Some(count)
 }
 
 #[cfg(test)]
@@ -84,6 +129,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(81));
     }
 }
