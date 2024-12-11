b8aca5b1586d64f6f134299bdc763d04ce2404f3
Fgdou <fabigoardou@gmail.com>
2024-12-11 18:41:08 +0000

    D11P2

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,21 +1,31 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(11);
 
-fn count_blink(n: usize, turns: usize) -> usize {
-    if turns == 0 {
-        return 1;
+fn count_blink(n: usize, turns: usize, cache: &mut HashMap<(usize, usize), usize>) -> usize {
+    if let Some(res) = cache.get(&(n, turns)) {
+        return *res;
     }
-    if n == 0 {
-        return count_blink(1, turns-1);
-    }
-    let str = n.to_string();
-    if str.len() % 2 == 0 {
-        let left = &str[0..str.len()/2].parse::<usize>().unwrap();
-        let right = &str[str.len()/2..str.len()].parse::<usize>().unwrap();
+    
+    let res = if turns == 0 {
+        1
+    } else if n == 0 {
+        count_blink(1, turns-1, cache)
+    } else {
+        let str = n.to_string();
+        if str.len() % 2 == 0 {
+            let left = &str[0..str.len()/2].parse::<usize>().unwrap();
+            let right = &str[str.len()/2..str.len()].parse::<usize>().unwrap();
+    
+            count_blink(*left, turns-1, cache) + count_blink(*right, turns-1, cache)
+        } else {
+            count_blink(n*2024, turns-1, cache)
+        }
+    };
 
-        return count_blink(*left, turns-1) + count_blink(*right, turns-1);
-    }
-
-    count_blink(n*2024, turns-1)
+    cache.insert((n, turns), res);
+    
+    res
 }
 
 type Input = Vec<usize>;
@@ -27,12 +37,16 @@ fn parse(input: &str) -> Input {
 
 pub fn part_one(input: &str) -> Option<usize> {
     let input = parse(input);
-    let sum = input.into_iter().map(|n| count_blink(n, 25)).sum();
+    let mut cache = HashMap::new();
+    let sum = input.into_iter().map(|n| count_blink(n, 25, &mut cache)).sum();
     Some(sum)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let input = parse(input);
+    let mut cache = HashMap::new();
+    let sum = input.into_iter().map(|n| count_blink(n, 75, &mut cache)).sum();
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -48,12 +62,11 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
     }
 
     #[test]
     fn test_count_blink(){
-        assert_eq!(count_blink(125, 1), 1);
-        assert_eq!(count_blink(125, 2), 2);
+        assert_eq!(count_blink(125, 1, &mut HashMap::new()), 1);
+        assert_eq!(count_blink(125, 2, &mut HashMap::new()), 2);
     }
 }
